00e7d74b0098f03c28d9be69a7130bd053b11705
hongbo <hongboshi@hongbos-MacBook-Pro.local>
2021-11-08 02:54:14 -0800

    change from R4 to R6 bin start_rothe.sh

--- BGPHijack/BGPHijacking/start_rogue.sh
+++ BGPHijack/BGPHijacking/start_rogue.sh
@@ -9,5 +9,5 @@ echo "Killing any existing rogue AS"
 ./stop_rogue.sh
 
 echo "Starting rogue AS"
-sudo python run.py --node R4 --cmd "/usr/lib/frr/zebra -f conf/zebra-R6.conf -d -i /tmp/zebra-R6.pid > logs/R6-zebra-stdout"
-sudo python run.py --node R4 --cmd "/usr/lib/frr/bgpd -f conf/bgpd-R6.conf -d -i /tmp/bgpd-R6.pid > logs/R6-bgpd-stdout"
+sudo python run.py --node R6 --cmd "/usr/lib/frr/zebra -f conf/zebra-R6.conf -d -i /tmp/zebra-R6.pid > logs/R6-zebra-stdout"
+sudo python run.py --node R6 --cmd "/usr/lib/frr/bgpd -f conf/bgpd-R6.conf -d -i /tmp/bgpd-R6.pid > logs/R6-bgpd-stdout"
--- BGPHijack/BGPHijacking/stop_rogue.sh
+++ BGPHijack/BGPHijacking/stop_rogue.sh
@@ -5,5 +5,5 @@
 # All rights reserved
 # Do not post or publish in any public or forbidden forums or websites
 
-sudo python run.py --node R4 --cmd "pkill -f --signal 9 [z]ebra-R6"
-sudo python run.py --node R4 --cmd "pkill -f --signal 9 [b]gpd-R6"
+sudo python run.py --node R6 --cmd "pkill -f --signal 9 [z]ebra-R6"
+sudo python run.py --node R6 --cmd "pkill -f --signal 9 [b]gpd-R6"
