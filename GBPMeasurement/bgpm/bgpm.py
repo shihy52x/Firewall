b50685bb53acf435c86d990937acebc57cab0102
hongbo <hongboshi@hongbos-MacBook-Pro.local>
2021-11-22 02:04:38 -0800

    task3Part1 finish with plot

--- GBPMeasurement/bgpm/bgpm.py
+++ GBPMeasurement/bgpm/bgpm.py
@@ -205,7 +205,7 @@ def calculateRTBHDurations(cache_files):
             if elem.type == "W":
                 print(elem)
                 print(elem.type)
-                print(elem.fields, elem.fields['communities'], elem.fields['prefix'], elem.peer_address)
+                #print(elem.fields, elem.fields['communities'], elem.fields['prefix'], elem.peer_address)
             path_string = elem._maybe_field("as-path")
             as_list = path_string.split(" ")
             as_set = set(as_list)
